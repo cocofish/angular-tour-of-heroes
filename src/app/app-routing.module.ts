230f0e69185c97b898df285b98e25c80229f4580
陈志强 <chenzhiqiang@panhaidata.com>
2018-04-29 09:56:23 +0800

    [+]官网学习

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HeroesComponent} from './views/hero/heroes/heroes.component';
+import {DashboardComponent} from 'app/views/dashboard/dashboard.component';
+import {HeroDetailComponent} from './views/hero/hero-detail/hero-detail.component';
+
+// 路由入口
+const routes: Routes = [
+  // 默认路由
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'heroes', component: HeroesComponent},
+  {path: 'detail/:id', component: HeroDetailComponent}
+];
+
+@NgModule({
+  imports: [
+    /** 提供路由所需的服务提供商和指令，还会基于浏览器的当前 URL 执行首次导航 */
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms'; // <-- NgModel lives here
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {AppComponent} from './app.component';
+import {HeroesComponent} from './views/hero/heroes/heroes.component';
+import {HeroDetailComponent} from './views/hero/hero-detail/hero-detail.component';
+import {MessagesComponent} from './views/message/messages.component';
+import {DashboardComponent} from './views/dashboard/dashboard.component';
 
+import {HeroService} from './service/hero.service';
+import {MessageService} from './service/message.service';
 
-import { AppComponent } from './app.component';
-
+import {AppRoutingModule} from './app-routing.module';
+import {HeroSearchComponent} from './views/hero/hero-search/hero-search.component';
 
 @NgModule({
+  // 声明
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+    HeroSearchComponent
   ],
+  // 外部模块
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  // 提供
+  providers: [
+    MessageService,
+    HeroService,
+    HttpClient
+  ],
+  // 元数据
+  schemas: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+// 声明了应用中的所有组件
+export class AppModule {
+}
